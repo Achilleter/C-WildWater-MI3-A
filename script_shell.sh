32fb5618e4c893d046364f172ce7f2d67543de15
Alexandre-bat <alexandrejeanbernard@outlook.fr>
2025-12-01 21:17:35 +0100

    Fix file argument and add existence check
    Corrected the file argument from $4 to $3 and added a check for file existence.

--- script_shell.sh
+++ script_shell.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # $4 correspond au quatrième argument dans la commande, $1 le premier ect.
-FICHIER="$4" 
+FICHIER="$3" 
+
+if [[ -f $FICHIER ]];then
+	echo "Le fichier en argument n'est pas correct"
+	exit 1
+fi
 
 # On identifie la demande pour renvoyer le résultat souhaité
 if [[ "$1" == "histo" ]]; then
@@ -11,6 +16,9 @@ if [[ "$1" == "histo" ]]; then
 		echo "on est dans histo src" #volume total capté par les sources
 	elif [[ "$2" == "real" ]]; then
 		echo "on est dans histo real" #volume total réellement traité
+	else
+		echo "L'argument après 'histo' n'a pas été compris"
+		exit 1
 	fi
 elif [[ "$1" == "leaks" ]]; then 
 	echo "on est dans leaks" # Pertes d'une usine passé en paramètre 3 ( $3 )
@@ -23,6 +31,7 @@ elif [[ "$1" == "HELP" ]]; then
 	- leaks 'REF_USINE' : mesure des pertes d’une usine  sur son réseau"
 else
 	echo "Aucun argument coresspondant, pour en savoir plus; exécuter avec comme premier argument: HELP"
+	exit 1
 fi
 
 #cat $FICHIER >> notes.txt # CECI EST TEMPORAIRE
