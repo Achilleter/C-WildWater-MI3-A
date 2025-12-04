554aa4e0273352fb30cd986f5dd7387f59290566
Alexandre-bat <alexandrejeanbernard@outlook.fr>
2025-12-04 10:52:13 +0100

    Implement verification functions for Infos structure
    Added verification functions for various conditions.

--- traiter.c
+++ traiter.c
@@ -6,3 +6,20 @@ File* incrémentationFICHIER( const char* nom ){
 	
 	
 }
+// Les fonctions suivantes vérifient les différentes lignes du fichier
+// Cette fonction vérifie : SOURCE -> USINE
+bool verif_S-U( Infos* i ){
+	return i->id_usine==NULL && i->id_amont!=NULL && i->id_aval!=NULL && i->amont!=NULL && i->fuites!=NULL
+}
+// Cette fonction vérifie : USINE
+bool verif_U( Infos* i ){
+	return i->id_usine==NULL && i->id_amont!=NULL && i->id_aval==NULL && i->amont!=NULL && i->fuites==NULL
+}
+// Cette fonction vérifie : USINE->STOCKAGE
+bool verif_U( Infos* i ){
+	return i->id_usine==NULL && i->id_amont!=NULL && i->id_aval!=NULL && i->amont==NULL && i->fuites!=NULL
+}
+// Cette fonction vérifie : STOCKAGE->JONCTION
+bool verif_U( Infos* i ){
+	return i->id_usine!=NULL && i->id_amont!=NULL && i->id_aval!=NULL && i->amont==NULL && i->fuites!=NULL
+}
