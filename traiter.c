7cb9ec9cac8eb4097b585eb7e700f6ac1079b0f1
Alexandre-bat <alexandrejeanbernard@outlook.fr>
2025-12-04 11:45:59 +0100

    Refactor incrémentationFICHIER for better data handling
    Refactor incrémentationFICHIER function to improve data extraction from file and fix issues with string comparison.

--- traiter.c
+++ traiter.c
@@ -1,11 +1,29 @@
 #include"Principal.h"
 
-File* incrémentationFICHIER( const char* nom ){
-	File* f = fopen( nom , r );
+// Fonction qui va nous perettre d'extraire les données du ficher
+FILE* incrémentationFICHIER( const char* nom ){
+	FILE* f = fopen( nom , "r" );
 	if( f == NULL ) exit(ERROR_FICHIER);
 	
+	char c1[64] ,c2[64] ,c3[64] ,c4[64] ,c5[64];
+	Infos i;
 	
+	while( fscanf(f , "%63[^;];%63[^;];%63[^;];%63[^;];%63[^\n]\n", c1,c2,c3,c4,c5) == 5 ){
+		
+		strcpy( i.id_usine , c1 );
+		strcpy( i.id_amont, c2 );
+		strcpy( i.id_aval , c3 );
+		
+		i.amont = (srtcmp( c4 , "-" )==0) ? -1.0 : atof(c4) ;
+		i.fuites = (srtcmp( c4 , "-" )==0) ? -1.0 : atof(c5) ;
+		
+		//printf(" %s | %s | %s | %f | %f \n", i.id_usine, i.id_amont, i.id_aval, i.amont, i.fuites); 
+	}
+	
+	fclose(f);	
+
 }
+
 // Les fonctions suivantes vérifient les différentes lignes du fichier
 // Cette fonction vérifie : SOURCE -> USINE
 bool verif_S-U( Infos* i ){
