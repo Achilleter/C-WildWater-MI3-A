841c47bedcf5d55fbc857ca9c4d7df225de1f441
Alexandre-bat <alexandrejeanbernard@outlook.fr>
2025-12-08 11:43:20 +0100

    Fix argument handling in script_shell.sh

--- script_shell.sh
+++ script_shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # $4 correspond au quatrième argument dans la commande, $1 le premier ect.
-FICHIER="$3" 
+FICHIER="$1" 
 
 if [[ -f $FICHIER ]];then
 	echo "Le fichier en argument n'est pas correct"
@@ -9,22 +9,22 @@ if [[ -f $FICHIER ]];then
 fi
 
 # On identifie la demande pour renvoyer le résultat souhaité
-if [[ "$1" == "histo" ]]; then
-	if [[ "$2" == "max" ]]; then
+if [[ "$2" == "histo" ]]; then
+	if [[ "$3" == "max" ]]; then
 		echo "on est dans histo max" #volume maximale de traitement de l’usine
-	elif [[ "$2" == "src" ]]; then
+	elif [[ "$4" == "src" ]]; then
 		echo "on est dans histo src" #volume total capté par les sources
-	elif [[ "$2" == "real" ]]; then
+	elif [[ "$4" == "real" ]]; then
 		echo "on est dans histo real" #volume total réellement traité
 	else
 		echo "L'argument après 'histo' n'a pas été compris"
 		exit 1
 	fi
-elif [[ "$1" == "leaks" ]]; then 
+elif [[ "$2" == "leaks" ]]; then 
 	echo "on est dans leaks" # Pertes d'une usine passé en paramètre 3 ( $3 )
     
 
-elif [[ "$1" == "HELP" ]]; then
+elif [[ "$2" == "HELP" ]]; then
 	echo "	- histo max : volume maximale de traitement de l’usine
  	- histo src : volume total capté par les sources
 	- histo real : volume total réellement trait
