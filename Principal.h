ea0750a84507358dcd5d74e61c590405dad619a9
Achille <achille.therin@etu.cyu.fr>
2025-12-08 11:28:18 +0100

    Leak probablement correct et structure avl

--- Principal.h
+++ Principal.h
@@ -5,17 +5,25 @@
 //Structures temporaires
 
 typedef struct{
-	char* id_usine ; //Colonne 1
-	char* id_amont ; //Colonne 2
-	char* id_aval ; //Colonne 3
+	char id_usine[64] ; //Colonne 1
+	char id_amont[64] ; //Colonne 2
+	char id_aval[64] ; //Colonne 3
 	float vol ; //Colonne 4 si prblm mettre en double
 	float fuites ; //Colonne 5
 } Infos;
 
 typedef struct Pile{
     Infos infos;
-    Pile* enfants;
+    struct Pile* enfants;
     struct Pile* next;
 } Pile;
+
+typedef struct avl_struct
+{
+    Pile* val;             // La valeur du nœud
+    int eq;                // Facteur d'équilibre (balance factor)
+    struct avl_struct *fg; // Pointeur vers le fils gauche
+    struct avl_struct *fd; // Pointeur vers le fils droit
+} AVL;
 
 //-------------------------------------------
--- leaks.c
+++ leaks.c
@@ -1,17 +1,16 @@
 #include "principal.h"
 
-void leaks(Usines* u, float leaktot){//leaktot doit etre initialisé a 0 avant l'appel
-    if (u==NULL){
-            printf("L'usine n'existe pas \n");
-            return -1;
+float leaks(Pile* u) {
+    if (u == NULL)
+        return 0
+    float total = 0;
+    while (u != NULL) {
+        // fuite du nœud courant
+        total += u->infos.vol * (u->infos.fuites / 100.0f);
+        // fuite des enfants
+        total += leaks(u->enfants);
+        // passer au frère suivant
+        u = u->next;
     }
-    while(u!=NULL){
-        if(u->infos.fuites>0){
-            leaktot+=u->infos.vol*u->(infos.fuites/100);
-        }
-        if(u->enfants.usine.enfants!=NULL){
-            leaks(u->enfants.usine);
-        }
-        u=u->next;
-    }
+    return total;
 }
