8f6028773bae7f9653bb092ba9cd9e0c87da3fe7
Alexandre-bat <alexandrejeanbernard@outlook.fr>
2025-12-16 14:52:10 +0100

    Correction

--- test.c
+++ test.c
@@ -1,4 +1,11 @@
 #include "Principal.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+
+// Forward declaration for tree cleanup
+void libererAVL(AVL* a);
 
 // ---------------------------------------------------------------------------------------------
 // Les fonctions suivantes vérifient les différentes lignes du fichier
@@ -82,13 +89,13 @@ AVL* doubleRotationDroite(AVL* a){
 }
 
 AVL* equilibrerAVL(AVL* a){
-    if(a->u.hauteur >= 2){
+    if(a->u.hauteur >= 2 && a->fd){
         if(a->fd->u.hauteur >= 0)
             return rotationGauche(a);
         else
             return doubleRotationGauche(a);
     }
-    else if(a->u.hauteur <= -2){
+    else if(a->u.hauteur <= -2 && a->fg){
         if(a->fg->u.hauteur <= 0)
             return rotationDroite(a);
         else
@@ -151,17 +158,25 @@ void afficherAVL(AVL* a){
            a->u.id, a->u.vol_max, a->u.vol_sources, a->u.vol_reel);
     afficherAVL(a->fd);
 }
+
+void libererAVL(AVL* a){
+	if(!a) return;
+	libererAVL(a->fg);
+	libererAVL(a->fd);
+	free(a->u.id);
+	free(a);
+}
 // ---------------------------------------------------------------------------------------------
 //	HISTO MAX
-void histo_maxMAX(AVL* Avl_U, Usine tab_max[]) {
+void histo_maxMAX(AVL* Avl_U, Usine* tab_max) {
 	if (Avl_U == NULL) return;
 
-	if (Avl_U->u.vol_max > tab_max[0].vol_max) {
+	if (tab_max[0].id == NULL || Avl_U->u.vol_max > tab_max[0].vol_max) {
 		tab_max[0] = Avl_U->u;
 
 		// Réordonner (croissant)
 		for (int i = 0; i < 9; i++) {
-			if (tab_max[i].vol_max > tab_max[i + 1].vol_max) {
+			if (tab_max[i].id != NULL && (tab_max[i + 1].id == NULL || tab_max[i].vol_max > tab_max[i + 1].vol_max)) {
 				Usine tmp = tab_max[i];
 				tab_max[i] = tab_max[i + 1];
 				tab_max[i + 1] = tmp;
@@ -173,35 +188,37 @@ void histo_maxMAX(AVL* Avl_U, Usine tab_max[]) {
 	histo_maxMAX(Avl_U->fd, tab_max);
 }
 
-void histo_maxMIN(AVL* Avl_U, Usine tab_min[]) {
-	if (Avl_U == NULL) return;
+void histo_maxMIN(AVL* Avl_U, Usine* tab_min) {
+    if (Avl_U == NULL) return;
 
-	if (Avl_U->u.vol_max < tab_min[0].vol_max) {
-		tab_min[0] = Avl_U->u;
+    if (Avl_U->u.vol_max < tab_min[49].vol_max) {
+        tab_min[49] = Avl_U->u;
 
-		// Réordonner (décroissant)
-		for (int i = 0; i < 49; i++) {
-			if (tab_min[i].vol_max < tab_min[i + 1].vol_max) {
-				Usine tmp = tab_min[i];
-				tab_min[i] = tab_min[i + 1];
-				tab_min[i + 1] = tmp;
-			}
-		}
-	}
+        // Réordonner pour que tab_min[0] ait le plus petit vol_max
+        for (int i = 48; i >= 0; i--) {
+            if (tab_min[i].vol_max > tab_min[i+1].vol_max) {
+                Usine tmp = tab_min[i];
+                tab_min[i] = tab_min[i+1];
+                tab_min[i+1] = tmp;
+            } else {
+                break; // optimisation
+            }
+        }
+    }
 
-	histo_maxMIN(Avl_U->fg, tab_min);
-	histo_maxMIN(Avl_U->fd, tab_min);
+    histo_maxMIN(Avl_U->fg, tab_min);
+    histo_maxMIN(Avl_U->fd, tab_min);
 }
 //	HISTO SRC
-void histo_srcMAX(AVL* Avl_U, Usine tab_max[]) {
+void histo_srcMAX(AVL* Avl_U, Usine* tab_max) {
 	if (Avl_U == NULL) return;
 
-	if (Avl_U->u.vol_sources > tab_max[0].vol_sources) {
+	if (tab_max[0].id == NULL || Avl_U->u.vol_sources > tab_max[0].vol_sources) {
 		tab_max[0] = Avl_U->u;
 
 		// Réordonner (croissant)
 		for (int i = 0; i < 9; i++) {
-			if (tab_max[i].vol_sources > tab_max[i + 1].vol_sources) {
+			if (tab_max[i].id != NULL && (tab_max[i + 1].id == NULL || tab_max[i].vol_sources > tab_max[i + 1].vol_sources)) {
 				Usine tmp = tab_max[i];
 				tab_max[i] = tab_max[i + 1];
 				tab_max[i + 1] = tmp;
@@ -213,35 +230,37 @@ void histo_srcMAX(AVL* Avl_U, Usine tab_max[]) {
 	histo_srcMAX(Avl_U->fd, tab_max);
 }
 
-void histo_srcMIN(AVL* Avl_U, Usine tab_min[]) {
-	if (Avl_U == NULL) return;
+void histo_srcMIN(AVL* Avl_U, Usine* tab_min) {
+    if (Avl_U == NULL) return;
 
-	if (Avl_U->u.vol_sources < tab_min[0].vol_sources) {
-		tab_min[0] = Avl_U->u;
+    if (Avl_U->u.vol_sources < tab_min[49].vol_sources) {
+        tab_min[49] = Avl_U->u;
 
-		// Réordonner (décroissant)
-		for (int i = 0; i < 49; i++) {
-			if (tab_min[i].vol_sources < tab_min[i + 1].vol_sources) {
-				Usine tmp = tab_min[i];
-				tab_min[i] = tab_min[i + 1];
-				tab_min[i + 1] = tmp;
-			}
-		}
-	}
+        // Réordonner pour que tab_min[0] ait le plus petit vol_sources
+        for (int i = 48; i >= 0; i--) {
+            if (tab_min[i].vol_sources > tab_min[i+1].vol_sources) {
+                Usine tmp = tab_min[i];
+                tab_min[i] = tab_min[i+1];
+                tab_min[i+1] = tmp;
+            } else {
+                break; // optimisation
+            }
+        }
+    }
 
-	histo_srcMIN(Avl_U->fg, tab_min);
-	histo_srcMIN(Avl_U->fd, tab_min);
+    histo_srcMIN(Avl_U->fg, tab_min);
+    histo_srcMIN(Avl_U->fd, tab_min);
 }
 //	HISTO REAL
-void histo_reelMAX(AVL* Avl_U, Usine tab_max[]) {
+void histo_reelMAX(AVL* Avl_U, Usine* tab_max) {
 	if (Avl_U == NULL) return;
 
-	if (Avl_U->u.vol_reel > tab_max[0].vol_reel) {
+	if (tab_max[0].id == NULL || Avl_U->u.vol_reel > tab_max[0].vol_reel) {
 		tab_max[0] = Avl_U->u;
 
 		// Réordonner (croissant)
 		for (int i = 0; i < 9; i++) {
-			if (tab_max[i].vol_reel > tab_max[i + 1].vol_reel) {
+			if (tab_max[i].id != NULL && (tab_max[i + 1].id == NULL || tab_max[i].vol_reel > tab_max[i + 1].vol_reel)) {
 				Usine tmp = tab_max[i];
 				tab_max[i] = tab_max[i + 1];
 				tab_max[i + 1] = tmp;
@@ -253,24 +272,26 @@ void histo_reelMAX(AVL* Avl_U, Usine tab_max[]) {
 	histo_reelMAX(Avl_U->fd, tab_max);
 }
 
-void histo_reelMIN(AVL* Avl_U, Usine tab_min[]) {
-	if (Avl_U == NULL) return;
+void histo_reelMIN(AVL* Avl_U, Usine* tab_min) {
+    if (Avl_U == NULL) return;
 
-	if (Avl_U->u.vol_reel < tab_min[0].vol_reel) {
-		tab_min[0] = Avl_U->u;
+    if (Avl_U->u.vol_reel < tab_min[49].vol_reel) {
+        tab_min[49] = Avl_U->u;
 
-		// Réordonner (décroissant)
-		for (int i = 0; i < 49; i++) {
-			if (tab_min[i].vol_reel < tab_min[i + 1].vol_reel) {
-				Usine tmp = tab_min[i];
-				tab_min[i] = tab_min[i + 1];
-				tab_min[i + 1] = tmp;
-			}
-		}
-	}
+        // Réordonner pour que tab_min[0] ait le plus petit vol_reel
+        for (int i = 48; i >= 0; i--) {
+            if (tab_min[i].vol_reel > tab_min[i+1].vol_reel) {
+                Usine tmp = tab_min[i];
+                tab_min[i] = tab_min[i+1];
+                tab_min[i+1] = tmp;
+            } else {
+                break; // optimisation
+            }
+        }
+    }
 
-	histo_reelMIN(Avl_U->fg, tab_min);
-	histo_reelMIN(Avl_U->fd, tab_min);
+    histo_reelMIN(Avl_U->fg, tab_min);
+    histo_reelMIN(Avl_U->fd, tab_min);
 }
 // ---------------------------------------------------------------------------------------------
 void incrementationFICHIER( const char* nom , const char* arg1 , const char* arg2){
@@ -286,11 +307,12 @@ void incrementationFICHIER( const char* nom , const char* arg1 , const char* arg
 
 	while( 1 ){
 		// Initialisation des chaînes de caractères à "-" avant le fscanf
-		strcpy(c1,"-");
-		strcpy(c2,"-");
-		strcpy(c3,"-");
-		strcpy(c4,"-");
-		strcpy(c5,"-");
+		strncpy(c1,"-", 64);
+		strncpy(c2,"-", 64);
+		strncpy(c3,"-", 64);
+		strncpy(c4,"-", 64);
+		strncpy(c5,"-", 64);
+		c1[63] = '\0'; c2[63] = '\0'; c3[63] = '\0'; c4[63] = '\0'; c5[63] = '\0';
 
 		int n = fscanf(f , "%63[^;];%63[^;];%63[^;];%63[^;];%63[^\n]", c1,c2,c3,c4,c5);
 
@@ -301,13 +323,13 @@ void incrementationFICHIER( const char* nom , const char* arg1 , const char* arg
 		int ch;
 		while ((ch = fgetc(f)) != '\n' && ch != EOF); // Nous permet de prendre la ligne au complet & fgetc nous permet de lire le caractère suivant sur la ligne
 		
-		strncpy( i.id_usine , c1, 63 );
+		strncpy( i.id_usine , c1, 64 );
 		i.id_usine[63] = '\0';
 
-		strncpy( i.id_amont, c2, 63 );
+		strncpy( i.id_amont, c2, 64 );
 		i.id_amont[63] = '\0';
 
-		strncpy( i.id_aval , c3, 63 );
+		strncpy( i.id_aval , c3, 64 );
 		i.id_aval[63] = '\0';
 		
 		i.vol = (strcmp( c4 , "-" )==0) ? -1.0 : atof(c4) ;
@@ -326,72 +348,106 @@ void incrementationFICHIER( const char* nom , const char* arg1 , const char* arg
 	if (strcmp(arg1, "histo") == 0) {
 			if (strcmp(arg2, "max") == 0) {
 				Usine tab_max[10];
-				for (int i = 0; i < 10; i++) tab_max[i].vol_max = -1;
+				for (int i = 0; i < 10; i++){
+					tab_max[i].vol_max = -1;
+					tab_max[i].id = NULL;
+				}
 
 				histo_maxMAX(u, tab_max);
 
 				Usine tab_min[50];
-				for (int i = 0; i < 50; i++) tab_min[i].vol_max = tab_max[0].vol_max;
+				for (int i = 0; i < 50; i++){
+					tab_min[i].vol_max = tab_max[9].vol_max;
+					tab_min[i].id = NULL;
+				}
 
 				histo_maxMIN(u, tab_min);
 				
 				
 				printf("HISTO MAX \nMax \n");
 				for( int i = 0 ; i<10 ; i++ ){
-					printf(" %d, %s , %.2f \n", i+1, tab_max[i].id, tab_max[i].vol_max);
+					if ( tab_max[i].id !=NULL ){
+						printf(" %d, %s , %.2f \n", i+1, tab_max[i].id, tab_max[i].vol_max);
+					}
 				}
 				printf("\n Min: \n");
 				for( int i = 0 ; i<50 ; i++ ){
-					printf(" %d, %s , %.2f \n", i+1, tab_min[i].id, tab_min[i].vol_max);
+					if ( tab_min[i].id !=NULL ){
+						printf(" %d, %s , %.2f \n", i+1, tab_min[i].id, tab_min[i].vol_max);
+					}
 				}
 			}
 			else if (strcmp(arg2, "src") == 0) {
 				Usine tab_max[10];
-				for (int i = 0; i < 10; i++) tab_max[i].vol_sources = -1;
+				for (int i = 0; i < 10; i++){
+					tab_max[i].vol_sources = -1;
+					tab_max[i].id = NULL;
+				}
 
 				histo_srcMAX(u, tab_max);
 
 				Usine tab_min[50];
-				for (int i = 0; i < 50; i++) tab_min[i].vol_sources = tab_max[0].vol_sources;
+				for (int i = 0; i < 50; i++){
+					tab_min[i].vol_sources = tab_max[9].vol_sources;
+					tab_min[i].id = NULL;
+				}
 
 				histo_srcMIN(u, tab_min);
 				
 				
 				printf("HISTO SRC \nMax \n");
 				for( int i = 0 ; i<10 ; i++ ){
-					printf(" %d, %s , %.2f \n", i+1, tab_max[i].id, tab_max[i].vol_sources);
+					if ( tab_max[i].id !=NULL ){
+						printf(" %d, %s , %.2f \n", i+1, tab_max[i].id, tab_max[i].vol_sources);
+					}
 				}
 				printf("\n Min: \n");
 				for( int i = 0 ; i<50 ; i++ ){
-					printf(" %d, %s , %.2f \n", i+1, tab_min[i].id, tab_min[i].vol_sources);
+					if ( tab_min[i].id !=NULL ){
+						printf(" %d, %s , %.2f \n", i+1, tab_min[i].id, tab_min[i].vol_sources);
+					}
 				}
 			}
 			else if (strcmp(arg2, "real") == 0) {
 				Usine tab_max[10];
-				for (int i = 0; i < 10; i++) tab_max[i].vol_reel = -1;
+				for (int i = 0; i < 10; i++){
+					tab_max[i].vol_reel = -1; 
+					tab_max[i].id = NULL;
+				}
 
 				histo_reelMAX(u, tab_max);
 
 				Usine tab_min[50];
-				for (int i = 0; i < 50; i++) tab_min[i].vol_reel = tab_max[0].vol_reel;
+				for (int i = 0; i < 50; i++){
+					tab_min[i].vol_reel= 1e9;
+					tab_min[i].id = NULL;
+				}
 
 				histo_reelMIN(u, tab_min);
 				
 				
-				printf("HISTO SRC \nMax \n");
+				printf("HISTO REAL \nMax \n");
 				for( int i = 0 ; i<10 ; i++ ){
-					printf(" %d, %s , %.2f \n", i+1, tab_max[i].id, tab_max[i].vol_reel);
+					if ( tab_max[i].id !=NULL ){
+						printf(" %d, %s , %.2f \n", i+1, tab_max[i].id, tab_max[i].vol_reel);
+					}
 				}
 				printf("\n Min: \n");
 				for( int i = 0 ; i<50 ; i++ ){
-					printf(" %d, %s , %.2f \n", i+1, tab_min[i].id, tab_min[i].vol_reel);
+					if ( tab_min[i].id != NULL ){
+						printf(" %d, %s , %.2f \n", i+1, tab_min[i].id, tab_min[i].vol_reel);
+					}
 				}
 			}
 		}
 	fclose(f);
+	libererAVL(u);
 }
 // ---------------------------------------------------------------------------------------------
 int main(int argc, char** argv) {
+	
+	clock_t start = clock();
+
 	if (argc < 4) {
 		fprintf(stderr, "Usage: %s \n", argv[0]);
 		return 0;
@@ -416,5 +472,12 @@ int main(int argc, char** argv) {
 	
 	
 	incrementationFICHIER(name, fonction , details );
+	
+	clock_t end = clock();
+
+	double temps = (double)(end - start) / CLOCKS_PER_SEC;
+	printf("Temps CPU : %.6f secondes\n", temps);
+	
+	
 	return 0;
 }
