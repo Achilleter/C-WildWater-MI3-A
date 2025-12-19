21f4bae65552ef58dafc9f54894aee81261a1a78
Alexandre-bat <alexandrejeanbernard@outlook.fr>
2025-12-19 14:15:49 +0100

    On passe de gnuplot.gnu a HISTOGRAM.py

---
+++ HISTOGRAM.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+
+plants = []
+values = []
+
+with open("histo.dat", "r") as f:
+    for line in f:
+        col1, col2, col3 = line.strip().split(",")
+        if col3 == "max":
+            plants.append(col1)
+            values.append(float(col2)/1000)
+
+plt.figure()
+plt.title("Meilleures Usines (MAX)")
+plt.xlabel("Plant IDs")
+plt.ylabel("Volumes (M.m3)")
+plt.bar(plants, values)
+plt.xticks(rotation=45, ha="right")
+plt.tight_layout()
+plt.show()
+
+plants = []
+values = []
+
+with open("histo.dat", "r") as f:
+    for line in f:
+        col1, col2, col3 = line.strip().split(",")
+        if col3 == "min":
+            plants.append(col1)
+            values.append(float(col2)/1000)
+
+plt.figure()
+plt.title("Pires Usines (MIN)")
+plt.xlabel("Plant IDs")
+plt.ylabel("Volumes (M.m3)")
+plt.bar(plants, values, color="orange")
+plt.xticks(rotation=45, ha="right")
+plt.tight_layout()
+plt.show()
